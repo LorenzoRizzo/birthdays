4f8519e0f2a07f149eb7b8663d7a1f4f6e4df867
Lorenzo Rizzo <lorenzo@Lorenzos-MacBook-Air-2.local>
2023-11-15 15:20:46 +0100

    modifyed file

--- birthdays.py
+++ birthdays.py
@@ -16,3 +16,20 @@ def return_birthday(name):
     else:
         print('Sadly, we don\'t have {}\'s birthday.'.format(name))
 
+'''
+ The goal of this module is to elevate our python and git abilities to a new level. 
+'''
+ birthdays = {
+     'Albert Einstein': '03/14/1879',
+     'Benjamin Franklin': '01/17/1706',
+     'Ada Lovelace': '12/10/1815',
+     'Donald Trump': '06/14/1946',
+     'Rowan Atkinson': '01/6/1955'}
+def print_birthdays():
+'''This function prints the name of the person if present in the data'''
+print('Welcome to the birthday dictionary:') for name in birthdays:
+print(name)
+def return_birthday(name): '''This function prints the birthday of a person present in the data given the name of the person''' if name in birthdays:
+        print('{}\'s birthday is {}.'.format(name, birthdays[name]))
+    else:
+        print('Sadly, we don\'t have {}\'s birthday.'.format(name))
